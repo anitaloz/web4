54fc08bcf4b1363775156954e5b598d4b1778d15
anitaloz <anitalozova@gmail.com>
2025-02-09 22:14:13 +0300

    commit

--- lab3/index.php
+++ lab3/index.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     print('Спасибо, результаты сохранены.');
   }
   // Включаем содержимое файла form.php.
-  include('MyProjects/Project1/index.html');
+  include('form.php');
   // Завершаем работу скрипта.
   exit();
 }

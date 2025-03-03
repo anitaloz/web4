a1e0467a95790b307c90548348a570e82489fde5
anitaloz <anitalozova@gmail.com>
2025-03-03 22:13:47 +0300

    commit

--- lab5/login.php
+++ lab5/login.php
@@ -28,7 +28,7 @@ function isValid($login, $db) {
 function password_check($login, $password, $db) {
   $passw;
   try{
-    $stmt = $db->prepare("SELECT pass FROM person_LOGIN WHERE login = :login");
+    $stmt = $db->prepare("SELECT pass FROM LOGIN WHERE login = :login");
     $stmt->bindParam(':login', $login, PDO::PARAM_STR);
     $stmt->execute();
     $passw = $stmt->fetchColumn();

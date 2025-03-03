48039430d2b49f7d91d833b07e9f44028840b074
anitaloz <anitalozova@gmail.com>
2025-03-03 22:00:08 +0300

    commit

--- lab5/login.php
+++ lab5/login.php
@@ -37,7 +37,7 @@ function password_check($login, $password, $db) {
     print('Error : ' . $e->getMessage());
     exit();
   }
-  return ($passw==$password);
+  return ($passw===$password);
 }
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.

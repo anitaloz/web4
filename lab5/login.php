d9efa4e653be80a9a03951df5909942cce8a8345
anitaloz <anitalozova@gmail.com>
2025-03-03 22:06:42 +0300

    commit

--- lab5/login.php
+++ lab5/login.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 // Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
   $login = $_POST['login'];
-  $password = md5($_POST['pass']);
+  $password = ($_POST['pass']);
 
   $user = 'u68598';
   $pass = '8795249';

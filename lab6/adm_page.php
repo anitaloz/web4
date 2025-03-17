5e4977ada3a2633e238ad3660a5427cbf82283a2
anitaloz <anitalozova@gmail.com>
2025-03-17 21:52:08 +0300

    commit

--- lab6/adm_page.php
+++ lab6/adm_page.php
@@ -1,6 +1,6 @@
 
 <?php
-$messAction=array()
+$messAction=array();
 if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] != 'admin' || md5($_SERVER['PHP_AUTH_PW']) != md5('123')) 
 {
     ?>

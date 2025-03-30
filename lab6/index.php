0f09651b5fd19081c21c09faf3f236b339cb81c9
anitaloz <anitalozova@gmail.com>
2025-03-30 16:50:39 +0300

    с

--- lab6/index.php
+++ lab6/index.php
@@ -223,6 +223,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   // ранее в сессию записан факт успешного логина.
   if(!empty($_GET['uid']))
   {
+    $user = 'u68598'; // Заменить на ваш логин uXXXXX
+    $pass = '8795249'; // Заменить на пароль
     $db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 

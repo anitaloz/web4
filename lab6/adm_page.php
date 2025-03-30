7872f6387772977a9836fb46b46a9f22360d2a66
anitaloz <anitalozova@gmail.com>
2025-03-30 18:59:07 +0300

    c

--- lab6/adm_page.php
+++ lab6/adm_page.php
@@ -1,5 +1,6 @@
-require_once 'db.php';
+
 <?php
+    require_once 'db.php';
     if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] != 'admin' || md5($_SERVER['PHP_AUTH_PW']) != md5('123')) 
     {
     header('HTTP/1.1 401 Unanthorized');

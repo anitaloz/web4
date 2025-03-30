806517c51daabbe9341f85c6de8f7fc5880b34e4
anitaloz <anitalozova@gmail.com>
2025-03-30 19:28:04 +0300

    commit

---
+++ lab6/functions.php
@@ -0,0 +1,18 @@
+<?php
+function password_check($login, $password, $db) {
+    $passw;
+    try{
+      $stmt = $db->prepare("SELECT pass FROM LOGIN WHERE login = :login");
+      $stmt->bindParam(':login', $login, PDO::PARAM_STR);
+      $stmt->execute();
+      $passw = $stmt->fetchColumn();
+      if($passw===false){
+        return false;
+      }
+      return password_verify($password, $passw);
+    } 
+    catch (PDOException $e){
+      print('Error : ' . $e->getMessage());
+      return false;
+    }
+  }

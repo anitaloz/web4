a71b531a57b784ce7351a7a3d70c07cddffe0b02
anitaloz <anitalozova@gmail.com>
2025-04-21 18:30:57 +0300

    Delete lab7/db.php

--- lab7/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $db;
-$user = 'u68598'; // Заменить на ваш логин uXXXXX
-$pass = '8795249'; // Заменить на пароль
-$db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
-[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-

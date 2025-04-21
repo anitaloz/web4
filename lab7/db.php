fd1f037e97cd0b0ac9ae28aa559bafb1f158068b
anitaloz <anitalozova@gmail.com>
2025-04-21 10:22:15 +0300

    Update db.php

--- lab7/db.php
+++ lab7/db.php
@@ -6,20 +6,3 @@ $pass = '8795249'; // Заменить на пароль
 $db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
 [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
-
-function findLoginByUid($update_id, $db)
-{
-    $update_query = "SELECT login FROM person_LOGIN WHERE id = :id";
-    try {
-        $update_stmt = $db->prepare($update_query);
-        $update_stmt->bindParam(':id', $update_id);
-        $update_stmt->execute();
-        $doplog=$update_stmt->fetchColumn();
-    }
-    catch (PDOException $e){
-        error_log('Database error: ' . $e->getMessage());
-        exit();
-    }
-    return $doplog;
-
-}
